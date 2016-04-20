78bc0a40171296748047080eb4ad8eaa96ee9278
Ariel <ariel@dapulse.com>
2016-04-20 11:32:50 +0300

    zooming at least 2x

--- Classes/IDMZoomingScrollView.m
+++ Classes/IDMZoomingScrollView.m
@@ -194,7 +194,7 @@
 	}
     
 	// Set
-	self.maximumZoomScale = maxScale;
+	self.maximumZoomScale = MAX(maxScale, 2); // at least 2x zoom scale
 	self.minimumZoomScale = minScale;
 	self.zoomScale = minScale;
     
